8c9c0750ea8a46d131fa7da61384ffb0ab48a472
HaruyaEnomto <e1j23014@oit.ac.jp>
2025-12-18 14:48:43 +0900

    ディレクトリパスの設定を修正し、Wiki関連のファイルパスを追加

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
 """プロジェクト設定""" 
 import os # ディレクトリ設定 
-DIR_CORPUS = "corpus"
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DIR_CORPUS = os.path.join(BASE_DIR, "corpus")
 DIR_DATA = "/home/aichatbot25/data"
-DIR_MODEL = "model"
+DIR_MODEL = os.path.join(BASE_DIR, "model")
+DIR_WIKI = os.path.join(BASE_DIR, "wiki")
+DIR_WIKI_CORPUS = os.path.join(DIR_WIKI, "corpus")
+DIR_WIKI_CORPUS_TEXT = os.path.join(DIR_WIKI_CORPUS, "text")
 
 # 入力ファイル 
 EXCEL_FILE = os.path.join(DIR_DATA, "FAQlist.xlsx")
+WIKI_FILE = os.path.join("/home/aichatbot25/book-local-llm-sample/src/ch4/anime_llm/corpus", "jawiki-latest-pages-articles-multistream.xml")
 
 # 生成ファイル 
-JSON_DATASET_FILE = os.path.join(DIR_CORPUS, "FAQdataset.json") 
+JSON_XLSX_DATASET_FILE = os.path.join(DIR_CORPUS, "FAQdataset.json") 
+JSON_MERGED_DATASET_FILE = os.path.join(DIR_CORPUS, "merged_data.json")
+
+JSON_WIKI_TARGET_FILES = os.path.join(DIR_WIKI_CORPUS, "hirakata_corpus_target_files.json")
+JSON_WIKI_DATASET_FILE = os.path.join(DIR_WIKI_CORPUS, "hirakata_corpus.json")
+JSON_TO_JSONL_FILE = os.path.join(DIR_WIKI_CORPUS, "train_data.json")
 
 
 # パラメータ 
@@ -17,3 +29,5 @@ OLLAMA_TIMEOUT = 30
 MAX_SEQ_LENGTH = 2048 
 MAX_STEPS = 500 
 MAX_DATASET_SIZE = 3000
+
+os.makedirs(DIR_WIKI_CORPUS_TEXT, exist_ok=True)
